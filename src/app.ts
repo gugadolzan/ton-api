f6d60675115874aa79f5974a3cf16e73153f4744
Gustavo Dolzan <gudolzan@gmail.com>
2022-03-15 18:29:30 -0300

    refactor: create src dir

--- src/app.ts
+++ src/app.ts
@@ -3,10 +3,7 @@ import express from "express";
 import ProductRouter from "./routes/Product";
 
 const app = express();
-const PORT = 3000;
 
 app.use("/", ProductRouter);
 
-app.listen(PORT, () => {
-  console.log(`Listening on port ${PORT}`);
-});
+export default app;
