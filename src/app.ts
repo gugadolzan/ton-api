c38ddb1e34adce9c4705783e330dd81dced8e093
Gustavo Dolzan <gudolzan@gmail.com>
2022-03-16 01:23:23 -0300

    feat: create error middleware

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,12 @@
 import express from "express";
 
 import ProductRouter from "./routes/Product";
+import error from "./middlewares/error";
 
 const app = express();
 
 app.use("/", ProductRouter);
+
+app.use(error);
 
 export default app;
---
+++ src/middlewares/error.ts
@@ -0,0 +1,10 @@
+import { NextFunction, Request, Response } from "express";
+
+/**
+ * Handle unexpected errors
+ */
+export default (err: Error, _req: Request, res: Response, _next: NextFunction) => {
+  console.log(err);
+
+  res.status(500).json({ message: "Internal server error" });
+};
