c42ff5a232496fca6d9af39c62e66335a702ed48
Gustavo Dolzan <gudolzan@gmail.com>
2022-03-16 09:58:10 -0300

    fix: set port environment variable

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import app from './app';
 
-const PORT = 3000;
+// `process.env.PORT` will be set by Heroku
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
   console.log(`Listening on port ${PORT}`);
