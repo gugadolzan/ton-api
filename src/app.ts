df6c32c10eb485747cd088a8ef98b54590c41d80
Gustavo Dolzan <gudolzan@gmail.com>
2022-03-16 14:44:15 -0300

    feat: install cors

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,12 @@
 import express from 'express';
+import cors from 'cors';
 
 import productRouter from './routes/product.route';
 import error from './middlewares/error';
 
 const app = express();
+
+app.use(cors());
 
 app.use('/', productRouter);
 
